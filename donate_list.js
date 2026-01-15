af51ff900403ace020a506b36a15bc45f6c4e70f
Shadik Hossain <mdshadikhossain830@gmail.com>
2026-01-15 11:35:46 +0600

    done

--- donate_list.js
+++ donate_list.js
@@ -12,7 +12,7 @@ function displayDonorList() {
 
   donorListContainer.innerHTML = ""; // Clear existing list
 
-  fetch("https://www.rural-peace.onrender.com/donors")
+  fetch("https://rural-peace.onrender.com/donors")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
